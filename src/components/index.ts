35730faf411bf885a6175dd9864014aaa51d4aad
damenor <damenor.develop@gmail.com>
2024-08-25 05:52:04 +0200

    Add favorites and new navbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as PokemonCard } from './PokemonCard.astro'
 export { default as PokemonPageHeader } from './PokemonPageHeader.astro'
 export { default as PokemonList } from './PokemonList.astro'
+
+export * from './shared'
